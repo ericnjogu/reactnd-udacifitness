b8e5f4e55ed4744a39f55b933abcad59ce2aa06f
Eric Njogu <kunadawa@gmail.com>
2019-05-06 14:35:34 +0300

    feat: rendering slider using the native control

--- components/UdaciSlider.js
+++ components/UdaciSlider.js
@@ -1,8 +1,16 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, Slider} from 'react-native'
 
-export default function UdaciSlider () {
+
+export default function UdaciSlider ({max, unit, step, value, onChange}) {
 	return (
-		<View><Text>Slider</Text></View>
+		<View>
+			<Slider step={step} value={value} maximumValue={max}
+			minimumValue={0} onValueChange={onChange}/>
+			<View>
+				<Text>{value}</Text>
+				<Text>{unit}</Text>
+			</View>
+		</View>
 	)
 }
