54ac00bb754ec241c971cbd22ca6260d2c43a3d6
Eric Njogu <kunadawa@gmail.com>
2019-05-06 12:19:00 +0300

    feat: adding components

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react'
 import {View, Text} from 'react-native'
 import {getMetricMetaInfo} from '../utils/helpers'
+import UdaciSlider from './UdaciSlider'
+import UdaciSteppers from './UdaciSteppers'
+import DateHeader from './DateHeader'
 
 export default class AddEntry extends Component {
 	state = {
@@ -41,9 +44,29 @@ export default class AddEntry extends Component {
 	}
 
 	render() {
+		const metaInfo = getMetricMetaInfo()
+
 		return (
 			<View>
-				{getMetricMetaInfo('eat').getIcon()}
+				<DateHeader date={(new Date()).toLocaleDateString()}/>
+				{Object.keys(metaInfo).map(key => {
+					const {getIcon, type, ...rest} = metaInfo[key]
+					const value = this.state[key]
+
+					return (<View key={key}>
+							{getIcon()}
+							{type === 'slider'
+							? <UdaciSlider
+								value={value}
+								onChange={(value) => this.slide(key, value)}/>
+							: <UdaciSteppers
+								value={value}
+								onIncrement={() => this.increment(key)}
+								onDecrement={() => this.decrement(key)}/>
+							}
+						</View>
+						)
+				})}
 			</View>
 		)
 	}
