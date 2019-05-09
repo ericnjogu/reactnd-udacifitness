63905e112dfb3594b5e510b0e03276228595ffab
Eric Njogu <kunadawa@gmail.com>
2019-05-10 02:47:59 +0300

    feat: adding bottom tab navigation with icons

--- App.js
+++ App.js
@@ -5,16 +5,34 @@ import {createStore} from 'redux'
 import {Provider} from 'react-redux'
 import reducer from './reducers'
 import History from './components/History'
+import {createBottomTabNavigator, createAppContainer} from 'react-navigation'
+import {FontAwesome, Entypo} from '@expo/vector-icons'
+
+const Tabs = createAppContainer(createBottomTabNavigator ({
+		History: {
+			screen: History,
+			navigationOptions: {
+				tabBarIcon: () => <FontAwesome name='history' size={30} color='black'/>
+			}
+		},
+		'Add Entry': {
+			screen:AddEntry,
+			navigationOptions: {
+				tabBarIcon: () => <Entypo name='add-to-list' size={30} color='black'/>
+			}
+		}
+	}))
 
 export default class App extends React.Component {
+
   render() {
+
     return (
     	<Provider store={createStore(reducer)}>
 	      <View style={{flex:1}}>
-	        {/*<AddEntry/>*/}
-	        <History/>
+	        <Tabs/>
 	      </View>
 	    </Provider>
-    );
+    )
   }
 }
