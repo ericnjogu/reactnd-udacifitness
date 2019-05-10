184e673e8e935b34e26490d3e464fa0113aa6734
Eric Njogu <kunadawa@gmail.com>
2019-05-10 04:57:53 +0300

    feat: reseting a day's activity detail and navigating to previous screen

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -3,6 +3,10 @@ import {View, Text, StyleSheet} from 'react-native'
 import {connect} from 'react-redux'
 import {white} from '../utils/colors'
 import MetricCard from './MetricCard'
+import {newEntry} from '../actions'
+import {removeEntry} from '../utils/api'
+import {timeToString, getDailyReminderValue} from '../utils/helpers'
+import TextButton from './TextButton'
 
 class EntryDetail extends React.Component {
 	static navigationOptions = ({navigation}) => {
@@ -12,27 +16,37 @@ class EntryDetail extends React.Component {
 		}
 	}
 
+	reset = () => {
+		const {remove, goBack, id} = this.props
+		remove()
+		goBack()
+		removeEntry(id)
+	}
 
 	render() {
 		const {metrics} = this.props
 		return (
 			<View style={styles.container}>
 				<MetricCard metrics={metrics}/>
+				<TextButton onPress={this.reset} style={{margin:20}}>Reset</TextButton>
 			</View>
 			)
+	}
+
+	shouldComponentUpdate(nextProps) {
+		return nextProps.metrics !== null && !nextProps.metrics.today
 	}
 }
 
 function mapStateToProps (state, {navigation}) {
 	const {id} = navigation.state.params
-	//debugger
 	return {
 		id,
 		metrics:state[id]
 	}
 }
 
-export default connect(mapStateToProps)(EntryDetail)
+export default connect(mapStateToProps, mapDispatchToProps)(EntryDetail)
 
 const styles = StyleSheet.create({
 	container: {
@@ -41,3 +55,16 @@ const styles = StyleSheet.create({
 		padding:15
 	}
 })
+
+function mapDispatchToProps (dispatch, {navigation}) {
+	const {id} = navigation.state.params
+
+	return {
+		remove: () => dispatch(newEntry({
+			[id]:timeToString() === id
+			? getDailyReminderValue
+			: null	
+		})),
+		goBack: () => navigation.goBack()
+	}
+}
