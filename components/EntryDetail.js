ce814e4b4fd2145ed54d1c561131e40f14b91157
Eric Njogu <kunadawa@gmail.com>
2019-05-10 04:32:53 +0300

    feat: reusing metric card component to show activity details

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,5 +1,8 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
+import {connect} from 'react-redux'
+import {white} from '../utils/colors'
+import MetricCard from './MetricCard'
 
 class EntryDetail extends React.Component {
 	static navigationOptions = ({navigation}) => {
@@ -11,12 +14,30 @@ class EntryDetail extends React.Component {
 
 
 	render() {
+		const {metrics} = this.props
 		return (
-			<View>
-				<Text>Entry Detail ({this.props.navigation.state.params.id})</Text>
+			<View style={styles.container}>
+				<MetricCard metrics={metrics}/>
 			</View>
 			)
 	}
 }
 
-export default EntryDetail
+function mapStateToProps (state, {navigation}) {
+	const {id} = navigation.state.params
+	//debugger
+	return {
+		id,
+		metrics:state[id]
+	}
+}
+
+export default connect(mapStateToProps)(EntryDetail)
+
+const styles = StyleSheet.create({
+	container: {
+		flex:1,
+		backgroundColor:white,
+		padding:15
+	}
+})
