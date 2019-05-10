b82ab3c44b6c442c2a9efa64c97bd7b8ebd4135c
Eric Njogu <kunadawa@gmail.com>
2019-05-10 04:08:13 +0300

    feat: adding navigation options to a 'target' component

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -2,6 +2,14 @@ import React from 'react'
 import {View, Text} from 'react-native'
 
 class EntryDetail extends React.Component {
+	static navigationOptions = ({navigation}) => {
+		const {id} = navigation.state.params
+		return {
+			title:new Date(Date.parse(id)).toLocaleDateString()
+		}
+	}
+
+
 	render() {
 		return (
 			<View>
